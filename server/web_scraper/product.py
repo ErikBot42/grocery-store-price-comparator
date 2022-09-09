4be4715d86332bdc4840f248abcb636b90724928
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-09-09 16:51:34 +0200

    coop in progress

--- server/web_scraper/product.py
+++ server/web_scraper/product.py
@@ -1,9 +1,27 @@
 # Product from the perspective of the web scraper
-import numbers
+
+from enum import Enum, unique
+@unique
+class Store(Enum):
+    LIDL = 1
+    COOP = 2
+    def __str__(self):
+        match self:
+            case self.LIDL:
+                return "Lidl"
+            case self.COOP:
+                return "Coop"
+
 
 class Product:
-    def __init__(self, name: str, price: numbers.Real):
-        self.price = price;
+    def __init__(self, name: str, price: float, store: Store):
+        assert name != ""
+        assert price > 0
+        self.price = price
         self.name = name
-
+        self.store = store
+    def __str__(self):
+        return self.name + ": " + str(self.price) + " kr at " + str(self.store)
+    def __repr__(self):
+        return self.name + ": " + str(self.price) + " kr at " + str(self.store)
 

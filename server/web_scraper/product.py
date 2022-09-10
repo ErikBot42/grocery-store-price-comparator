8a0c38961d2eb7b79c26e55abfd413136e0edbc6
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-09-10 12:31:03 +0200

    web scraper ICA

--- server/web_scraper/product.py
+++ server/web_scraper/product.py
@@ -1,27 +1,33 @@
 # Product from the perspective of the web scraper
+# This WILL have to be parsed later to provide useful info
+# TODO: images
 
 from enum import Enum, unique
 @unique
 class Store(Enum):
     LIDL = 1
     COOP = 2
+    ICA = 3
     def __str__(self):
         match self:
             case self.LIDL:
                 return "Lidl"
             case self.COOP:
                 return "Coop"
-
+            case self.ICA:
+                return "Ica"
 
 class Product:
-    def __init__(self, name: str, price: float, store: Store):
-        assert name != ""
-        assert price > 0
+    #TODO: filter valid products
+    def __init__(self, name: str, price: str, store: Store, description: str = "", category: str = ""):
+        #assert name != ""
         self.price = price
         self.name = name
         self.store = store
+        self.description = description
+        self.category = category
     def __str__(self):
-        return self.name + ": " + str(self.price) + " kr at " + str(self.store)
+        return self.name + ": (" + self.price + ") at " + str(self.store) + " " + self.description
     def __repr__(self):
-        return self.name + ": " + str(self.price) + " kr at " + str(self.store)
+        return self.name + ": (" + self.price + ") at " + str(self.store) + " " + self.description
 
--- server/web_scraper/web_scraper.py
+++ server/web_scraper/web_scraper.py
@@ -2,6 +2,7 @@ import requests
 import re
 import product
 from bs4 import BeautifulSoup
+import bs4
 
 #does this string contain amount, eg "Ca 200g", or not, eg "Klass 1"
 def is_amount(amount_str: str) -> bool:
@@ -11,8 +12,6 @@ def is_amount(amount_str: str) -> bool:
 #remove surrounding whitespace and empty elements
 def remove_whitespace_elements(original: list[str]) -> list[str]:
     return [el.strip() for el in original if len(el.strip()) != 0]
-
-
 
 def filter_price_string(price_string: str) -> str:
     return re.sub(r'[^\d.,]*', '', price_string.strip())
@@ -25,72 +24,106 @@ def safe_request(http_str: str) -> bytes:
     except requests.exceptions.ConnectionError:
         return bytes()
 
+# None -> "", other -> other.strip()
+def safe_none_str(maybe_string: None | str) -> str:
+    if maybe_string == None:
+        return ""
+    else:
+        return maybe_string.strip()
+
+# get string safetly
+def soup_safe_str(maybe_soup: BeautifulSoup | bs4.Tag | bs4.NavigableString | None) -> str:
+    if maybe_soup == None:
+        #assert False
+        return ""
+    elif isinstance(maybe_soup, bs4.NavigableString):
+        return safe_none_str(maybe_soup)
+    return safe_none_str(maybe_soup.string)
+
+def soup_find(soup: BeautifulSoup, kind: str, class_tag: str) -> bs4.Tag | bs4.NavigableString | None:
+    return soup.find(kind, class_=class_tag)
+
 # make request from http address and return soup object
 def address_to_soup(address: str) -> BeautifulSoup:
-    content = safe_request(address)
+    content: bytes = safe_request(address)
     return BeautifulSoup(content, 'html.parser')
 
 # Parse lidl offers from html
 def lidl_parse(soup: BeautifulSoup) -> list[product.Product]:
-    s = soup.find_all('div', class_ = "nuc-a-flex-item nuc-a-flex-item--width-6 nuc-a-flex-item--width-4@sm")
+    s: list[BeautifulSoup] = soup.find_all('div', class_ = "nuc-a-flex-item nuc-a-flex-item--width-6 nuc-a-flex-item--width-4@sm")
     product_list: list[product.Product] = []
     for el in s:
         #TODO: Image link
-        product_price_el = el.find('span', class_ = "lidl-m-pricebox__price");
-        product_name_el = el.find('h3', class_ = "ret-o-card__headline");
-        price_str = filter_price_string(product_price_el.string)
-        name_str = product_name_el.string.strip();
-        print("name:", name_str)
-        print("price:", price_str)
-        print()
-        product_list.append(product.Product(name_str, float(price_str), product.Store.LIDL))
+        product_price_el = el.find('span', class_ = "lidl-m-pricebox__price")
+        product_name_el = el.find('h3', class_ = "ret-o-card__headline")
+        price_str: str = filter_price_string(soup_safe_str(product_price_el))
+        name_str: str = soup_safe_str(product_name_el)
+        product_list.append(product.Product(
+            name=name_str, 
+            price=price_str, 
+            store=product.Store.LIDL))
     return product_list
 
 #<div class="ItemTeaser-content">
 #<div class="Grid-cell u-size1of2 u-xsm-size1of2 u-md-size1of4 u-lg-size1of6 js-drOffer js-offerItem" data-eag-id="12960" data-store-id="165420">
 #<article class="ItemTeaser" itemscope="" itemtype="http://schema.org/Product">
 def coop_parse(soup: BeautifulSoup) -> list[product.Product]:
-    #s = soup.find_all('div', class_ = "ItemTeaser-info")
     s = soup.find_all('article', class_ = "ItemTeaser")
+    product_list: list[product.Product] = []
     for el in s:
-        #print(el.prettify())
-        #assert False
-        print("image link:", el.find('img', class_ = "u-posAbsoluteCenter").get('src'))
-        price_raw = remove_whitespace_elements(list(el.find('span', class_ = "Splash-content").strings))
         #['3 för', '79:-']
         #['50%', 'rabatt']
         #['49', '90', '/st']
-        print("price (sometimes):", price_raw)
-        print("heading:", el.find('h3', class_ = "ItemTeaser-heading").string)
-        #print("brand:", el.find('span', class_ = "ItemTeaser-brand").string)
-        description_list = list(el.find('p', class_ = "ItemTeaser-description").strings);
-        description_list = remove_whitespace_elements(description_list)
-        brand: str = "" 
-        description: str = ""
-        amount: str = "" 
-        try:
-            brand = description_list[0]
-            description_split = description_list[1].split(".")
-            maybe_amount: str = description_split[0]
-            if is_amount(maybe_amount):
-                amount = maybe_amount
-                description_split.pop(0)
-            description = ".".join(description_split)
-        except IndexError:
-            pass
+        price_raw: str = " ".join(remove_whitespace_elements(list(el.find('span', class_ = "Splash-content").strings)))
+        heading: str = el.find('h3', class_ = "ItemTeaser-heading").string
+        description: str = " ".join(remove_whitespace_elements(list(el.find('p', class_ = "ItemTeaser-description").strings)));
+        #print("image link:", el.find('img', class_ = "u-posAbsoluteCenter").get('src'))
+        #print("price:", price_raw)
+        #print("heading:", heading)
+        #print("description:", description)
+        #print()
+        product_list.append(product.Product(
+            name=heading, 
+            price=price_raw, 
+            store=product.Store.COOP,
+            description=description))
+    return product_list
 
-        print("brand:", brand)
-        print("description:", description)
-        print("amount:", amount)
-        print()
-    
+def ica_parse(soup: BeautifulSoup) -> list[product.Product]:
+    #<section class="offer-category details open">
+    offer_groups: list[BeautifulSoup] = soup.find_all('section', class_ = "offer-category details open")
+    product_list: list[product.Product] = []
+    for offer_group in offer_groups:
+        header_soup = offer_group.find('header', class_ = "offer-category__header summary active")
+        category: str = soup_safe_str(header_soup)
+        offers = offer_group.find_all('div', class_="offer-category__item")
+        print("category:", category, "offers:", len(offers))
+        for offer in offers:
+            title: str = soup_safe_str(offer.find('h2', class_="offer-type__product-name splash-bg icon-store-pseudo"))
+            description: str = soup_safe_str(offer.find('p', class_="offer-type__product-info"))
+            price: str = soup_safe_str(offer.find('div', class_="product-price__price-value"))\
+            + " " +      soup_safe_str(offer.find('div', class_="product-price__decimal"))\
+            + " " +      soup_safe_str(offer.find('div', class_="product-price__unit-item benefit-more-info"))
+            price = price.strip()
+            #print(offer.prettify())
+            #exit()
 
+            print("    title:", title)
+            print("    description:", description)
+            print("    price:", price)
+            product_list.append(product.Product(
+                name=title, 
+                price=price, 
+                store=product.Store.ICA,
+                description=description,
+                category=category))
 
-#
-soup = address_to_soup('https://www.coop.se/butiker-erbjudanden/coop/coop-kronoparken/')
-coop_parse(soup)
+    return product_list
+
+soup = address_to_soup('https://www.ica.se/butiker/maxi/orebro/maxi-ica-stormarknad-universitetet-orebro-15088/erbjudanden/')
+print(ica_parse(soup))
+#soup = address_to_soup('https://www.coop.se/butiker-erbjudanden/coop/coop-kronoparken/')
 #print(coop_parse(soup))
-soup = address_to_soup('https://www.lidl.se/veckans-erbjudanden')
-lidl_parse(soup)
+#soup = address_to_soup('https://www.lidl.se/veckans-erbjudanden')
 #print(lidl_parse(soup))
 

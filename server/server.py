6cfffb03e857a3d3ae21f8c9243da3edb75feb50
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-09-14 19:19:56 +0200

    New Example
    +Example of o function
    +Explaining comments

--- server/server.py
+++ server/server.py
@@ -1,24 +1,38 @@
+from itertools import count
 from pickle import NONE
 import sqlite3
-print("This machine can run sqlite3")
 
-con = sqlite3.connect("Grocery_Store_Database.db")
-cur = con.cursor()
-res = cur.execute("SELECT * FROM Register")
-print(res.fetchall())
-con.commit()
-con.close()
-
-#TODO Add product, Add user, add store, Loggin
+#Databasen "Grocery_Store_Database.db" måste ligga i samma map för att kunna köra koden(Ligger på Discord)
+#TODO Add product, add store, Loggin
 
 def AddProductToDatabase():
-    print("TODO")
+    print("TODO: AddProductToDatabase")
 
-def AddUserToDatabase():
-    print("TODO")
+#Adds a user to the database. Default values are only ment for testing
+def AddUserToDatabase(ID: int,email: str, mobile_nr: int, name: str = "TestName", password: str = "Password", date_of_birth: int = 19900101, city: str = "Karlstad", country: str = "Sweden", status: int = 0):
+    try:
+        cur.execute("INSERT INTO Register (User_ID, Name, Email, Password, Mobile_Number,  Date_of_Birth, City, Country, Logged_in_Status) VALUES ('"+str(ID)+"', '"+name+"', '"+email+"', '"+password+"', '"+str(mobile_nr)+"', '"+str(date_of_birth)+"', '"+city+"', '"+country+"', '"+str(status)+"')")
+        #cur.execute("INSERT INTO Register (User_ID, Name, Email, Password, Mobile_Number,  Date_of_Birth, City, Country, Logged_in_Status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (ID, name, email, password, mobile_nr, date_of_birth, city, country, status))
+        #TODO: fix safe imput. Example -> cur.execute("select * from register where name =  %s", (input))
+    except:
+        print("\nUnable to add user\n")
 
 def AddStoreToDatabase():
-    print("TODO")
+    print("TODO: AddStoreToDatabase")
 
-def Loggin():
-    print("TODO")
+def Loggin() -> bool:
+    print("TODO: Loggin")
+
+#Save all new changes to the database. 
+def CommitToDatabase():
+    con.commit()
+
+
+con = sqlite3.connect("Grocery_Store_Database.db")  #Conection to database
+cur = con.cursor()                                  #cursor executes sql comands
+AddUserToDatabase(ID = 7, email = "test7@email.com", mobile_nr = 1415917)   #Testar att lägga till en användare i databasen
+res = cur.execute("SELECT * FROM Register")         
+print("Users:")   
+for result in res:  print(result)
+CommitToDatabase()          
+con.close()

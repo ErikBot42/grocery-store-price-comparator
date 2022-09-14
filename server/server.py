90c09ea682e6926dec05c7c29a9158bffaa21ea5
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-09-14 11:42:02 +0200

    Databas test

--- server/server.py
+++ server/server.py
@@ -1,4 +1,10 @@
+from pickle import NONE
 import sqlite3
 print("This machine can run sqlite3")
 
-
+con = sqlite3.connect("Test2.db")
+cur = con.cursor()
+res = cur.execute("SELECT * FROM REGISTER")
+print("Print 1")
+print(res.fetchall())
+print("Print 2")

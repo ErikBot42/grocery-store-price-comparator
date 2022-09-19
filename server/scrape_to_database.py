2e45252e703209d21517b4ef93aa0f88fd70073c
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-09-19 20:14:34 +0200

    Nya funktioner
    AddProductDataForAdmin() ger namn, pris och butik på produkterna.
    Nu omvandlas en butiks namn till rätt id när en produkt läggs till

--- server/scrape_to_database.py
+++ server/scrape_to_database.py
@@ -2,4 +2,5 @@ from web_scraper import add_all_to_database
 from server import Database
 data = Database()
 add_all_to_database(data)
-
+#data.commitToDatabase() #Lägg till för att spara data till databasen
+data.Close() 

f8a8d3711661db433e15d4cc0a6d003b704e2bfe
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-09-21 11:13:51 +0200

    ee

--- server/fill_database_with_placeholders.py
+++ server/fill_database_with_placeholders.py
@@ -2,7 +2,19 @@ from database import Database
 
 
 database = Database()
-database.fillDatabase()
+#database.fillDatabase()
 #database.droppAllData()
+database.cursor.execute("DROP TABLE Product")
+database.cursor.execute("""CREATE TABLE "Product" (
+	"Category_ID"	INTEGER,
+	"Product_ID"	INTEGER,
+	"Product_Name"	TEXT,
+	"Store_ID"	INTEGER,
+	"Price"	TEXT,
+	"URL"	TEXT,
+	PRIMARY KEY("Product_ID"),
+	FOREIGN KEY("Store_ID") REFERENCES "Store"("Store_ID"),
+	FOREIGN KEY("Category_ID") REFERENCES "Category"("Category_ID")
+)""")
 database.commitToDatabase()
 database.Close()

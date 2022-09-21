d11f80dcd7f46830f4f2151f745d9b3f1a349be8
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-09-21 09:45:04 +0200

    rename: server.py -> database.py

--- server/fill_database_with_placeholders.py
+++ server/fill_database_with_placeholders.py
@@ -1,4 +1,4 @@
-from server import Database
+from database import Database
 
 
 database = Database()
--- server/scrape_to_database.py
+++ server/scrape_to_database.py
@@ -1,5 +1,5 @@
 from web_scraper import add_all_to_database
-from server import Database
+from database import Database
 data = Database()
 add_all_to_database(data)
 

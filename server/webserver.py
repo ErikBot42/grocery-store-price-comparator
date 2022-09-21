780af32d0ebed4410ace6f3cae6a254459543422
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-09-21 10:44:35 +0200

    website from database

--- server/webserver.py
+++ server/webserver.py
@@ -1,20 +1,38 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from database import Database
 #import time
 
+database = Database()
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        assert self.command == "GET"
+        attrs = vars(self)
+        for item in attrs.items():
+            print("%s: %s" % item)
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        file = open("website/"+self.path)
+        file_content = file.read()
+        if self.path == "/adminview.html":
+            file_content = file_content.replace("ADD_THINGS_HERE", 
+            " ".join([database.getProductString(l) for l in database.getProductDataForAdmin()]))
+        self.wfile.write(bytes(file_content,'utf-8'))
+        #self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        #self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        #self.wfile.write(bytes("<body>", "utf-8"))
+        #self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        #self.wfile.write(bytes("</body></html>", "utf-8"))
+        
+    #TODO
+    def do_POST(self):
+        assert self.command == "POST"
+        pass
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)

eeba15e03f271169fefb350c2a62223ab1cc1e5e
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-09-28 13:29:01 +0200

    Added Favourite_Store till databasen + safe SQL

--- server/database.py
+++ server/database.py
@@ -12,20 +12,20 @@ class Database:
 
     def _createInsertSQLQuery(self, table: str, categories: str, values: list[str]) -> str:
         query = f"INSERT INTO {table} ({categories}) VALUES ("
-        query = query + "'" + values.pop(0) + "'"
-        for value in values:
-            value = value.replace("'", "")
-            query = query + ", '" + value + "'"
-        query = query + ")"
+        query = f"{query} ?"
+        for value in range(len(values) - 1):
+            query = f"{query}, ?"
+        query = f"{query} )"
         return query
 
-    def _runInsertSQLQuerry(self, query: str) -> bool:
+    def _runSQLQuerry(self, query: str, data: list[str]) -> bool:
         try:
-            self.cursor.execute(query)
+            self.cursor.execute(query, data)
             return True
         except sqlite3.Error as er:
             print("\nCould not run querry: " + query)
             print('\tSQLite error: %s' % (' '.join(er.args)))
+            print("Om du saknar tabeller, testa att uppdatera databasen med Database.recreateDatabase()")
             return False
 
     def _runSQLQueryWhitResults(self, query: str) -> list:
@@ -38,69 +38,77 @@ class Database:
             return []
 
     def addProductToDatabase(self, name: str, store: str, price: str, category: int, url: str = "") -> bool:
+        data = [str(category), name, str(self.getStoreID(store)), price, url]
         query = self._createInsertSQLQuery(
             "Product", 
             "Category_ID, Product_Name, Store_ID, Price, URL", 
-            [str(category), name, str(self.getStoreID(store)), price, url]
+            data
             )
-        return self._runInsertSQLQuerry(query)
+        return self._runSQLQuerry(query, data)
     
     def addUserToDatabase(self, email: str, mobile_nr: int, name: str = "TestName", password: str = "Password", date_of_birth: int = 19900101, city: str = "Karlstad", country: str = "Sweden", status: int = 0) -> bool:
+        data = [name, email, password, str(mobile_nr), str(date_of_birth), city, country, str(status)]
         query = self._createInsertSQLQuery(
             "Register",
             "Name, Email, Password, Mobile_Number, Date_of_Birth, City, Country, Logged_in_Status",
-            [name, email, password, str(mobile_nr), str(date_of_birth), city, country, str(status)]
+            data
         ) 
-        return self._runInsertSQLQuerry(query)
+        return self._runSQLQuerry(query, data)
     
     def addStoreToDatabase(self, ID: int, name: str) -> bool:
+        data = [str(ID), name]
         query = self._createInsertSQLQuery(
             "Store",
             ("Store_ID, Store_Name"),
-            [str(ID), name]
+            data
         )
-        return self._runInsertSQLQuerry(query)
+        return self._runSQLQuerry(query, data)
     
     def addCategoryToDatabase(self, ID: int, name: str) -> bool:
+        data = [str(ID), name]
         query = self._createInsertSQLQuery(
             "Category",
             "Category_ID, Category_Name",
-            [str(ID), name]
+            data
         )
-        return self._runInsertSQLQuerry(query)
+        return self._runSQLQuerry(query, data)
 
 
     def addFavoriteProduct(self, user_ID: int, product_ID: int) -> bool:
+        data = [str(user_ID), str(product_ID)]
         query = self._createInsertSQLQuery(
-        "Favourite_Products",
-        "User_ID, Product_ID",
-        [str(user_ID), str(product_ID)]
-        ) 
-        return self._runInsertSQLQuerry(query)
+            "Favourite_Products",
+            "User_ID, Product_ID",
+            data
+            ) 
+        return self._runSQLQuerry(query, data)
 
     def addShopingList(self, list_name) -> bool:    #NOTE AutoIncrement needs to be added to List table in database, fix PascalCase on List.List_Name
+        data = [list_name]
         query = self._createInsertSQLQuery(
-        "List",
-        "List_Name",
-        [list_name]
-        ) 
-        return self._runInsertSQLQuerry(query)
+            "List",
+            "List_Name",
+            data
+            ) 
+        return self._runSQLQuerry(query, data)
 
     def addShopingListOwner(self, user_ID: int, list_ID: int) -> bool: #NOTE Fix PascalCase on tale name: List_Owner
+        data = [str(user_ID), str(list_ID)]
         query = self._createInsertSQLQuery(
-        "List_Owner",
-        "User_ID, List_ID",
-        [str(user_ID), str(list_ID)]
-        ) 
-        return self._runInsertSQLQuerry(query)
+            "List_Owner",
+            "User_ID, List_ID",
+            data
+            ) 
+        return self._runSQLQuerry(query, data)
 
     def addShopingListItem(self, list_ID: int, product_ID: int, amount: int) -> bool: #NOTE Fix PascalCase on List_Items.List_ID
+        data = [str(list_ID), str(product_ID), str(amount)]
         query = self._createInsertSQLQuery(
-        "List_Items",
-        "List_ID, Product_ID, Amount",
-        [str(list_ID), str(product_ID), str(amount)]
-        ) 
-        return self._runInsertSQLQuerry(query)
+            "List_Items",
+            "List_ID, Product_ID, Amount",
+            data
+            ) 
+        return self._runSQLQuerry(query, data)
 
 
     def logginValidation(self, email: str, password: str) -> bool:
@@ -125,12 +133,13 @@ class Database:
             return False
 
     def addFavoriteStore(self, user_ID: int, store_ID: int) -> bool:
+        data = [str(user_ID), str(store_ID)]
         query = self._createInsertSQLQuery(
-        "Favourite_Store",
-        "User_ID, Store_ID",
-        [str(user_ID), str(store_ID)]
-        ) 
-        return self._runInsertSQLQuerry(query)
+            "Favourite_Store",
+            "User_ID, Store_ID",
+            data
+            ) 
+        return self._runSQLQuerry(query, data)
 
     def getProductDataForAdmin(self):
         query = "SELECT Product_Name, Price, Store_Name, Product_ID FROM Product JOIN Store USING (Store_ID)"
@@ -151,7 +160,10 @@ class Database:
         query = f"DELETE FROM Product WHERE Product_ID == '?'"
         data = [str(id)]
         print(f"TODO: Handle exeptions in removeProduct")
-        self.cursor.execute(query, data)
+        try:
+            self.cursor.execute(query, data)
+        except sqlite3.Error:
+            print("Error in removing product: TODO: HAndle error in removeProduct")
 
 
     def getProductString(self, values: list):
@@ -235,8 +247,8 @@ class Database:
         self.cursor.execute("DROP TABLE Product")
         self.cursor.execute("DROP TABLE Register")
         self.cursor.execute("DROP TABLE Store")
-
-
+        self.cursor.execute("DROP TABLE Favourite_Store")
+        
         self.cursor.execute("""CREATE TABLE "Favourite_Products" (
                 "User_ID"	INTEGER,
                 "Product_ID"	INTEGER,
@@ -309,10 +321,26 @@ class Database:
                 PRIMARY KEY("Store_ID")
             )
         """)
+
+        self.cursor.execute("""CREATE TABLE "Favourite_Store" (
+                "Store_ID"	INTEGER,
+                "User_ID"	INTEGER,
+                FOREIGN KEY("User_ID") REFERENCES "Register"("User_ID"),
+                FOREIGN KEY("Store_ID") REFERENCES "Store"("Store_ID"),
+                PRIMARY KEY("Store_ID","User_ID")
+            )
+        """)
         self.addStoreToDatabase(ID = 1, name = "LIDL")
         self.addStoreToDatabase(ID = 2, name = "COOP")
         self.addStoreToDatabase(ID = 3, name = "ICA")
         self.addStoreToDatabase(ID = 4, name = "WILLYS")
         self.commitToDatabase()
+
+
+db = Database()
+db.recreateDatabase()
+db.fillDatabase()
+db.commitToDatabase()
+db.close()
 
 

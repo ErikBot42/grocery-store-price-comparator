52b3e007accb5ba0b384a07197149170dc0ded2b
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-10-04 10:58:44 +0200

    lex

--- server/product.py
+++ server/product.py
@@ -21,6 +21,73 @@ class Store(Enum):
             case self.WILLYS:
                 return "WILLYS"
 
+
+class ExtractedInfo:
+    def __init__(self):
+        self.price = ""
+        pass
+    def try_read(self, string: str):
+        import ply.lex as lex
+
+        # List of token names.   This is always required
+        tokens = (
+            #'NUMBER',
+           'PRICE',
+           'PER_KG',
+           #'MINUS',
+           #'TIMES',
+           #'DIVIDE',
+           #'LPAREN',
+           #'RPAREN',
+        )
+        
+        # Regular expression rules for simple tokens
+        t_PRICE = r'(\d+[ .]\d\d)|(\d+:-)'
+        t_PER_KG
+        #t_MINUS   = r'-'
+        #t_TIMES   = r'\*'
+        #t_DIVIDE  = r'/'
+        #t_LPAREN  = r'\('
+        #t_RPAREN  = r'\)'
+        
+        # A regular expression rule with some action code
+        #def t_NUMBER(t):
+        #    r'\d+'
+        #    t.value = int(t.value)
+        #    return t
+        
+        # Define a rule so we can track line numbers
+        #def t_newline(t):
+        #    r'\n+'
+        #    t.lexer.lineno += len(t.value)
+        
+        # A string containing ignored characters (spaces and tabs)
+        t_ignore  = ' \t\n'
+        
+        # Error handling rule
+        def t_error(t):
+            print("Illegal character '%s'" % t.value[0])
+            t.lexer.skip(1)
+        
+        # Build the lexer
+        lexer = lex.lex()
+
+        print() 
+        print(string)
+        # Give the lexer some input
+        lexer.input(string)
+        
+        # Tokenize
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break      # No more input
+
+            print(tok.type, ": " , tok.value, sep="")
+
+
+
+
 class Product:
     #TODO: create function that may return product if it's valid
     def __init__(self,

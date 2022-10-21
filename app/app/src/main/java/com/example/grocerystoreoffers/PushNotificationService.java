19ca707ce9eeba9f46a6fb185bf203d05d12c2b3
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2022-10-21 09:01:59 +0200

    Notification

--- app/app/src/main/java/com/example/grocerystoreoffers/PushNotificationService.java
+++ app/app/src/main/java/com/example/grocerystoreoffers/PushNotificationService.java
@@ -1,16 +1,6 @@
 package com.example.grocerystoreoffers;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationManagerCompat;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
 
 public class PushNotificationService extends FirebaseMessagingService {
 }
